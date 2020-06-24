733b38af69bca73e964a36ced6806618cc0d9362
Lisa Feng <lisafeng10@gmail.com>
2020-06-24 03:46:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,12 @@
-class Shoe
+class Shoe(brand)
+  def initialize(brand)
+    @brand = brand
+  end
   
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  
+  def cobble(shoe)
+    shoe.condition = 
+  end
 end
