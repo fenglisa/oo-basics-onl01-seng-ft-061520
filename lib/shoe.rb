09147eb0dae1967eb5fb5107075ddfa3378944a2
Lisa Feng <lisafeng10@gmail.com>
2020-06-24 03:49:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,5 @@ class Shoe(brand)
   attr_reader :brand
   attr_accessor :color, :size, :material, :condition
   
-  def cobble(shoe)
-    puts "Your shoe is as good as new!"
-    shoe.condition = "new"
-  end
+  
 end
